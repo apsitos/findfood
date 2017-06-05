4f66aa72e4ff32206eea3a0925f40a4086122c2f
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-06-05 16:34:27 -0600

    Attempt to add geocode api

---
+++ src/controller.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const config = require('../config.env');
+const API_KEY = (config.API_KEY);
+
+function getPlaces() {
+  const address_url = `https://maps.googleapis.com/maps/api/geocode/json?address=90+Corona+St,+Denver,+CO&key=${API_KEY}`
+  request(address_urlL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+  })
+}
+
+// function getPlaces(req, res, next) {
+//   let { address } = req.body
+//   let lat
+//   let long
+//   lat = lat || 39.7257155
+//   long = long || -104.9713034
+//   const BASE_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=1600&keyword=restaurant&pagetoken&key=${API_KEY}`
+//
+//
+//   request(BASE_URL, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       res.send(body)
+//     }
+//   })
+// }
+
+module.exports = {
+  getPlaces: getPlaces
+};
--- src/router.js
+++ src/router.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 var controller = require('./controller');
 
-router.get('/address', controller.getAddress)
-.then(router.get('/places', controller.getPlaces))
+router.get('/places', controller.getPlaces)
 
 module.exports = router;
-
-
-// || call getAddress in the controller file
