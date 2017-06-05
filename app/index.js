10c575acfa2b773b470f216fcbbc23a7c84d9547
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-06-05 16:14:00 -0600

    Begin adding code for app.js

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+import Main from './Components/Main/Main';
+import Location from './Components/Location/Location'
+
+render(
+    <Router history={hashHistory}>
+      <Route path="/" component={Main}>
+        <Route path='location' component={Location}/>
+      </Route>
+    </Router>
+  , document.querySelector('.application'))
