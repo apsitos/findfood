2d9642a47e66d5957489141554f496f9817d4bd7
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-06-06 15:52:16 -0600

    Basic React components rendering

---
+++ app/Components/Main/Main.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Landing from '../Landing/Landing';
+import Location from '../Location/Location';
+import Button from '../Button/Button';
+
+export default class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      lat = '',
+      long = '',
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <Header />
+        <Landing />
+        <Button id='search-btn' handleClick={() => this.showBars()} name='Find Food!'/>
+        <Location />
+      </div>
+    )
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+// import { BrowserRouter, Match, Miss, Link } from 'react-router';
 import Main from './Components/Main/Main';
-import Location from './Components/Location/Location'
+// import Location from './Components/Location/Location'
 
-render(
-    <Router history={hashHistory}>
-      <Route path="/" component={Main}>
-        <Route path='location' component={Location}/>
-      </Route>
-    </Router>
-  , document.querySelector('.application'))
+
+render(<Main />, document.querySelector('.application'))
