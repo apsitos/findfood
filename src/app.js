9d7a4233074194147b538c82ea57a3a6ca85d230
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-06-07 13:17:12 -0600

    We'll see

--- src/app.js
+++ src/app.js
@@ -34,10 +34,32 @@ app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '/../build/
 
 function getPlaces(req, res, next) {
   const address_url = `https://maps.googleapis.com/maps/api/geocode/json?address=90+Corona+St,+Denver,+CO&key=${API_KEY}`
+  let lat
+  let long
+
   request(address_url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(response.body);
-      // response.send(response.body)
+      geoLoc = JSON.parse(response.body)
+      geoLoc.results.map((results) => {
+        lat = results.geometry.location.lat
+        long = results.geometry.location.lng
+        return lat, long
+      })
+    }
+    console.log(lat, long);
+  })
+  getPlaces(req, res, lat, long)
+}
+
+function getPlaces(req, res, lat, long) {
+  lat = lat || 39.7257155
+  long = long || -104.9713034
+  const BASE_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=1600&keyword=restaurant&pagetoken&key=${API_KEY}`
+
+  request(BASE_URL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body)
+>>>>>>> Stashed changes
     }
   })
   .then((response) => { response.sendFile(path.join(__dirname, '/../build/index.html')) })
--- src/controller.js
+++ src/controller.js
@@ -11,6 +11,7 @@ function getPlaces() {
   })
 }
 
+<<<<<<< Updated upstream
 // function getPlaces(req, res, next) {
 //   let { address } = req.body
 //   let lat
@@ -26,6 +27,20 @@ function getPlaces() {
 //     }
 //   })
 // }
+=======
+function getPlaces(req, res, next) {
+  let { address } = req.body
+  lat = lat || 39.7257155
+  long = long || -104.9713034
+  const BASE_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=1600&keyword=restaurant&pagetoken&key=${API_KEY}`
+
+  request(BASE_URL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body)
+    }
+  })
+}
+>>>>>>> Stashed changes
 
 module.exports = {
   getPlaces: getPlaces
