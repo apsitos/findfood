d84b30263f29991a4433119af2e31ec90d4454d0
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-06-18 12:53:33 -0600

    Attempt to use String prototype .replace

--- app/Components/Main/Main.js
+++ app/Components/Main/Main.js
@@ -8,10 +8,25 @@ export default class Main extends Component {
   constructor() {
     super()
     this.state = {
-      lat: '',
-      long: '',
+      address: '',
       barArray: []
     }
+    // this.showBars.bind(this)
+    // this.getAddress.bind(this)
+  }
+
+  saveAddress(e) {
+    this.setState({ address: e.target.value})
+  }
+
+  getAddress() {
+    // let splitAddress = this.state.address.split(',')
+    let streetAddress = this.state.address
+    debugger
+    for(let i = 0; i < streetAddress.length; i++) {
+      streetAddress.replace(/\s/, '+')
+    }
+    console.log(streetAddress);
   }
 
   showBars() {
@@ -20,6 +35,7 @@ export default class Main extends Component {
       return response.json()
     })
     .then((data) => {
+      console.log(data.results);
       this.setState({ barArray: data.results });
     })
     .catch((error) => {
@@ -32,7 +48,15 @@ export default class Main extends Component {
       <div>
         <Header />
         <Landing />
-        <Button id='search-btn' handleClick={this.showBars.bind(this)} name='Find Food!'/>
+        <div className='address'>
+          <input type='text'
+                name='address'
+                value={this.state.address}
+                placeholder='ex. 123 Main St, Anywhere, MO'
+                onChange={(e) => this.saveAddress(e)}
+          />
+        </div>
+        <Button id='search-btn' handleClick={this.getAddress.bind(this)} name='Find Food!'/>
         <Location bars={this.state.barArray} />
       </div>
     )
